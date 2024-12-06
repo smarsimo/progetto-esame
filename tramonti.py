2af2675d6f989baea32f9245d5ab29745ebaada5
smarsimo <simonuccimarco#gmail.com>
2024-12-06 11:55:00 +0100

    modified a file

--- tramonti.py
+++ tramonti.py
@@ -12,24 +12,69 @@ T_s  = 5.75e3 # K
 T_a  = 4e3    # K
 T_sp = 18e3   # K
 
-#numero di fotoni
-N_fot=10000
-N_estr=10000
-lmax = 2e-6
-lmin = 1e-7
-lt=np.linspace(lmin,lmax,N_estr)
-ld=np.random.uniform(lmin,lmax,N_estr)
+def parse_arguments():
+	parser = argparse.ArgumentParser(description='simulazione diffusione di fotoni', usage='python3 tramonti.py --opzione')
+	parser.add_argument('--solsc', action='store_true', help='produce un grafico della densità dei fotoni solari secondo teoria e altri tre grafici che simulano la distribuzione degli stessi in tre differenti casi')
+	
+	
+	return parser.parse_args()
 
 def scaled_den(l,T):
+	"""
+	funzione densità dei fotoni scalata in maniera tale che
+	il massimo valore corrisponda ad 1
+	"""
 	return funzioni.den_fot(l,T)/max(funzioni.den_fot(l,T))
+
+def tramonti():
 	
-y_i = np.random.random(N_estr)
+	args = parse_arguments()
 	
-mask= y_i <= scaled_den(ld, T_s)
+	if args.solsc == True:
 
-x_i=ld[mask]
+		N_estr=10000
+		lmax = 2e-6
+		lmin = 1e-7
+		
+		lt=np.linspace(lmin,lmax,N_estr)
+		den=funzioni.den_fot(lt,T_s)
+		
+		#grafico della densità dei fotoni che arrivano dal sole
+		plt.plot(lt,den,color='royalblue')
+		plt.suptitle("grafico della densità in funzione della lunghezza d'onda")
+		plt.xlabel(r'$\lambda [\mu m]$')
+		plt.ylabel('densità dei fotoni [$m^-3$]')
+		plt.show()
+		
+		#faccio un'estrazione della lunghezza d'onda nel dominio
+		#di definizione, utilizzo il metodo hit or miss
+		ld=np.random.uniform(lmin,lmax,N_estr)
+		
+		#estraggo anche la densità nell'intervallo [0,1]
+		d_i = np.random.random(N_estr)
+		
+		#utilizzo un maschera per selezionare le lunghezze d'onda giuste
+		mask= d_i <= scaled_den(ld, T_s)
+		l_i=ld[mask]
 
-plt.hist(x_i,bins=100,color='blue',ec='darkblue')
-plt.suptitle('distribuzione dei fotoni che arrivano in caso di non assorbimento')
-plt.show()	
+		#stampo le lunghezze d'onda estratte
+		print("lunghezze d'onda estratte:", l_i)
+		
+		#grafico della distribuzione dei fotoni che arrivano in caso di
+		#non assorbimento
+		n,bins,_=plt.hist(l_i,bins=100,color='blue',ec='darkblue')
+		plt.suptitle('distribuzione dei fotoni che arrivano in caso di non assorbimento')
+		plt.xlabel(r'${\lambda}[\mu m]$')
+		plt.show()	
+		
+		#creo una lista dove immagazzinare il numero di fotoni NON
+		#diffusi nel caso in cui il cielo si trovasse allo zenith
+		print(len(n))
+		print(n)
+		print(len(bins))
+		print(bins)
+		
 
+
+if __name__ == "__main__":
+	tramonti()
