b2cde1f3e4fe7d93b920f89f4ecda2bad60b856a
Stefano Simonucci <stefano@BULLSEYE-debian-live-builder>
2024-12-22 15:18:06 +0100

    modified and added files

--- StellaX.py
+++ StellaX.py
@@ -15,11 +15,13 @@ X_st  = pd.read_csv('202302/observed_starX.csv')
 theta = 13*np.pi/36 # angolo zenitale (65°)
 R_ter = 6.378e6  #m Raggio della Terra 
 lo    = X_st['lambda (nm)'] 
-ld    = lo*10**(-9)
+ld    = lo*10**(-9) #m
 ph    = X_st['photons']
 n_ter = 1.00029  # indice di rifrazione dell'aria
 N_ter = 2.504e25 # mol/m^3
 
+#divido l'array contenente il numero di fotoni per il valore massimo
+#contenuto nell'array stesso in modo da ricavare una densità "osservata"
 m_p=max(ph)
 den_abs = ph/m_p #densità dei fotoni (dati del file)
 
@@ -29,6 +31,7 @@ print(X_st.columns)
 plt.plot(ld, den_abs, 'o', color='royalblue')
 plt.xlabel(r'$\lambda$ (nm)')
 plt.ylabel('den_fotoni')
+#plt.savefig('immagini/grafico_dati_osservati.jpeg')
 plt.show()
 
 
@@ -47,17 +50,30 @@ print('temperatura ricavata attraverso il fit della stella:', p)
 y = fit_fun(ld, p)
 plt.subplots(figsize=(9,6))
 
-#test del chi2
-chi2 = np.sum((y-den_abs)**2 /den_abs)
-ndf  = len(ld)-len(p)
-print("\u03C7^2 / ndf = ",chi2/ndf )
+error=np.sqrt(pcov[0][0])
 
+#inizializzo una maschera in maniera tale da selezionare soltanto i
+#den_abs diversi da zero, per avere un valore finito del chi2,
+#che altrimenti divergerebbe
+mask = den_abs != 0
+oss = den_abs[mask]
+ass = y[mask]
+
+#test del chi2
+chi2 = np.sum((ass-oss)**2 /oss)
+ndf  = len(ld[mask])-len(p)
+#stampo sul terminale il valore del chi2
+print("\u03C7^2 / ndf = {:.4f}".format(chi2/ndf) )
+
+#stampo il grafico del fit 
 plt.plot(ld, den_abs, 'o', color='royalblue', label='punti del file')
 plt.plot(ld, y, color='limegreen', label='curva del fit')
 plt.xlabel(r'$\lambda$ (nm)')
 plt.ylabel('densità fotoni')
-plt.text(2e-6,0.8, 'T={:}'.format(p[0]), fontsize=16)
+plt.text(2e-6,0.8, r'T={:.0f} $\pm$ {:.0f} K'.format(p[0],error), fontsize=16)
+plt.text(2e-6,0.7, r'$\chi$/ndf = {:.4f}'.format(chi2/ndf), fontsize=16)
 plt.suptitle('fit dei punti forniti')
+#plt.savefig('immagini/fit_funzione.jpeg')
 
 plt.legend()
 plt.show()
